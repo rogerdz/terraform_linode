ab48979e41d8a4164416186561c28a8b7a8d501b
Hieu Dang <danghieu14@gmail.com>
2022-12-26 13:42:15 +0700

    update pub key

--- main.tf
+++ main.tf
@@ -17,6 +17,11 @@ variable "NAME" {
   default = "Name"
 }
 
+variable "JENKINS_AGENT_SSH_PUBKEY" {
+  description = "Public key ssh"
+  default = "key"
+}
+
 provider "linode" {
   token = var.TOKEN
 }
@@ -41,7 +46,7 @@ resource "linode_instance" "jenkins-agent" {
 
 	provisioner "remote-exec" {
     inline = [
-      "docker run -d --rm --name=agent --publish 2020:22 -v /var/run/docker.sock:/var/run/docker.sock -e 'JENKINS_AGENT_SSH_PUBKEY=ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDG/U3Iyz299qasZxKWwWgx96Qfng4+s2zrwqwlZAuFRcmsRc1r10kSLDYmWUviJc0gjdu+4BrnGxx8CNG+7SuAh5I2MJXlquBrht29Xi72BZOjCn9eMmKg6OtybRDMCakpkf0Ji2I5oYinEip8IdBE8ZOM/C/l6t+0fmIlct8GSGknVvPBHglpuTbO9ow6pTM1XyNj+sUuzPLwyZq20B+aX+aUGfv+ryk1Y+zT7BPupKgQa0dm8lEOMHmR57iyFDe1PIzqM3PfDuDMOKnxIfTOQYTRIzi4Ve2fC4bMnReyfWcBjVHtrcmJUwxpJJBcQxkxfvo7hATT1vBP8dErGlT2PwKPTc1E90Plf/l44rWcVyLxgl9ISEQA/7nZnABD7xGKXUiEPNKlwl2xxKhqxgiQm1mA0YExqHqQ6LrV1MHMOmXN2orM/hsWXzU0BmXyGG/YxY9jWp5jzr1/fPhsQzWufJruD6uDhbAum2bMUPPVHcIBx8t8Js1mZTQo7UUvJTAHWUca7zlCfXoR86NWGdIyhZsCjL30tDf8H7GHHOVu8m0EbTTAzD71Se1viAvpgRD7jbmtIPXTnWBMQonWQDm0XBr3AW9ybvrZFSYf3DCJ483T8zAW/RR9WGUXTDjvW9oZk79aVK9gEsFPsn/idv82xSJAYR3TgbVUwTv6TCEjjQ== hieudang@hieudang' rogerdz/jenkins:ssh-agent"
+      "docker run -d --rm --name=agent --publish 2020:22 -v /var/run/docker.sock:/var/run/docker.sock -e 'JENKINS_AGENT_SSH_PUBKEY=var.JENKINS_AGENT_SSH_PUBKEY' rogerdz/jenkins:ssh-agent"
     ]
 	}
 }
